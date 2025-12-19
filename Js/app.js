dd2ae9b3429a98f7a5bd32682bae18fc9d4624b4
EselALEYOUKNOW <marttyno.0508@gmail.com>
2025-12-18 23:10:14 -0600

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -12,22 +12,22 @@ const db = getFirestore(app);
 
 const contenedor = document.getElementById("alumnos");
 
-const snapshot = await getDocs(collection(db, "alumnos"));
+const snapshot = await getDocs(collection(db, "Alumnos"));
 
-snapshot.forEach(doc => {
-  const alumno = doc.data();
+snapshot.forEach((docSnap) => {
+  const alumno = docSnap.data();
 
   const card = document.createElement("div");
   card.className = "card";
 
   card.innerHTML = `
-    <img src="${alumno.fotoURL}">
+    <img src="fotos/${alumno.fotoURL}">
     <h3>${alumno.nombre}</h3>
     <p>🔒 Click → Login</p>
   `;
 
   card.addEventListener("click", () => {
-    window.location.href = `login.html?id=${doc.id}`;
+    window.location.href = `login.html?id=${docSnap.id}`;
   });
 
   contenedor.appendChild(card);
